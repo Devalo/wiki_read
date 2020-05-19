942cf24cdc2e83e7d109af3db6eccf23deb338aa
Stephan Valois <stephan@devalo.no>
2020-05-19 14:45:57 +0000

    l

--- application.py
+++ application.py
@@ -15,7 +15,6 @@ def index():
 
     articles = lookup()
 
-    print(articles)
     return render_template("index.html", articles=articles)
 
 
@@ -24,6 +23,7 @@ def index():
 def lookup():
     random_url = "https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=json"
     article_url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&pageids="
+    image_url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=images&pageids="
     response = json.loads(requests.get(random_url).text)
     response = response.get("query").get("random")
 
@@ -34,8 +34,13 @@ def lookup():
 
     for article in response:
         text_url = f"{article_url}{article['id']}"
+        image = f"{image_url}{article['id']}"
+
         parsed_text = json.loads(requests.get(text_url).text)
         parsed_text = parsed_text.get("query").get("pages").get(str(article["id"])).get("extract")
+
+        parsed_image = json.loads(requests.get(image).text)
+        print(parsed_image)
 
         articles.update( {article["id"]: [article["title"], parsed_text]} )
 
