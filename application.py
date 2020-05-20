0353c8b8007ef5128b3e001fd359c61e725a5c5c
Stephan Valois <stephan@devalo.no>
2020-05-20 06:40:28 +0000

    Final

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
-from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, render_template, request
 import requests
 import json
 
@@ -7,40 +6,56 @@ app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-db = SQL("sqlite:///wiki_read.db")
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    number = 10
 
-    articles = lookup()
+    if request.method == "POST":
+        number = request.form.get("amount")
+        if number == "":
+            number = 10
 
-    return render_template("index.html", articles=articles)
+        articles = lookup(number)
+
+        return render_template("index.html", articles=articles)
+
+    else:
+        articles = lookup()
+        return render_template("index.html", articles=articles)
 
 
 
 
-def lookup():
-    random_url = "https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=json"
+def lookup(amount = 10):
+    # Set up urls to hit wikipedia API
+    random_url = f"https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit={amount}&format=json"
     article_url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&pageids="
     image_url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=images&pageids="
-    
+
+    # Create main api call
     response = json.loads(requests.get(random_url).text)
     response = response.get("query").get("random")
 
+    # Dictionary to build the pack served to the tempate
     articles = {}
 
 
 
     for article in response:
+        # Urls to get main text and image from API
         text_url = f"{article_url}{article['id']}"
         image = f"{image_url}{article['id']}"
 
+        # Parse json to collect pure text
         parsed_text = json.loads(requests.get(text_url).text)
         parsed_text = parsed_text.get("query").get("pages").get(str(article["id"])).get("extract")
 
-        parsed_image = json.loads(requests.get(image).text)
 
+        #Collect image
+        # Build article with image, title, text
+        parsed_image = json.loads(requests.get(image).text)
         if parsed_image.get("query").get("pages").get(str(article['id'])).get("images") != None:
             if "File" and "jpg" or "png" in parsed_image.get("query").get("pages").get(str(article['id'])).get("images")[0].get("title"):
                 filename = parsed_image.get("query").get("pages").get(str(article['id'])).get("images")[0].get("title")
@@ -50,12 +65,6 @@ def lookup():
             else:
                 articles.update( {article["id"]: [article["title"], parsed_text]} )
 
-        
+
     return articles
 
-
-# https://commons.wikimedia.org/wiki/Special:FilePath/Ad-tech_London_2010_(2).JPG?width=200 
-# https://commons.wikimedia.org/wiki/Special:FilePath/Wiki_letter_w.svg?width=200 
-
-# https://commons.wikimedia.org/wiki/Special:FilePath/Ad-tech_London_2010_(2).JPG
-# https://commons.wikimedia.org/wiki/Special:FilePath/Flag-map_of_Nebraska.svg
---
+++ project/README.md
@@ -0,0 +1,9 @@
+# Wiki Rand
+
+With this program, you are able to get served random Wikipedia articles to read.
+The program will hit the Wikipedia API, and pull down as many articles as the user wants.
+
+The articles will contain the headline, a truncated text from the main summary of the Wikipedia article,
+and also a picture if available. The user can go directly to the original Wikipedia article.
+
+All articles get rendered with in a card
